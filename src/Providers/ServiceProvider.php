28982acf0215142b7c140de48b513929b10b84a3
Tran Ngoc Duc <caothu91@gmail.com>
2024-07-03 12:43:49 +0700

    Class "Diepxuan\Log\ServiceProvider" not found

--- src/Providers/ServiceProvider.php
+++ src/Providers/ServiceProvider.php
@@ -8,7 +8,7 @@ declare(strict_types=1);
  * @author     Tran Ngoc Duc <ductn@diepxuan.com>
  * @author     Tran Ngoc Duc <caothu91@gmail.com>
  *
- * @lastupdate 2024-07-03 11:31:57
+ * @lastupdate 2024-07-03 12:46:45
  */
 
 namespace Diepxuan\Log\Providers;
